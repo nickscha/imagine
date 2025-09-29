8bbea9b54cba0e1150e667be2052029e6f3b9336
nickscha <nick@scharnbeck.de>
2025-09-29 10:11:01 +0200

    [VERSION] bump to v0.2

--- tests/imagine_test.c
+++ tests/imagine_test.c
@@ -1,4 +1,4 @@
-/* imagine.h - v0.1 - public domain data structures - nickscha 2025
+/* imagine.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) Image Library (IMAGINE).
 
